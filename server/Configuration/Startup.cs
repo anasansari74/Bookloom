c352996e721d4c52233504b27a793bbdc22ade20
Anas <anas_ansar1@outlook.com>
2024-07-10 18:48:26 +0100

    Configure Dependency Injection Services in Startup.cs

---
+++ server/Configuration/Startup.cs
@@ -0,0 +1,22 @@
+namespace Server.Configuration
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure services
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure middleware
+        }
+    }
+}
